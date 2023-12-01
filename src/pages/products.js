7779951171fcc750e1a2ee2aa710f313232e91ea
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-12-01 16:24:20 +0700

    Add products HTML and finished search function

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,6 +3,11 @@ import "../toggleSidebar.js";
 import "../cart/toggleCart.js";
 import "../cart/setupCart.js";
 
+//  filter imports
+import setupSearch from "../filters/search.js";
+import setupCompanies from "../filters/companies.js";
+import setupPrice from "../filters/price.js";
+
 // specific imports
 import { store } from "../store.js";
 import display from "../displayProducts.js";
@@ -11,5 +16,9 @@ import { getElement } from "../utils.js";
 const loading = getElement(".page-loading");
 
 display(store, getElement(".products-container"));
+
+setupSearch(store);
+setupCompanies(store);
+setupPrice(store);
 
 loading.style.display = "none";

ef5e6aa04ab970ec33be5c1c4df08a981f40dd6a
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-12-01 11:31:56 +0700

    Display featured products

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import "./src/cart/toggleCart.js";
 // specific imports
 import fetchProducts from "./src/fetchProducts.js";
 import { setupStore, store } from "./src/store.js";
+import display from "./src/displayProducts.js";
+import { getElement } from "./src/utils.js";
 
 const init = async () => {
   const products = await fetchProducts();
   if (products) {
     // add products to the store
     setupStore(products);
+    const featured = store.filter((product) => product.featured === true);
+    display(featured, getElement(".featured-center"));
   }
 };
 
---
+++ src/displayProducts.js
@@ -0,0 +1,30 @@
+const display = (products, element) => {
+  // display products
+  element.innerHTML = products
+    .map((product) => {
+      const { id, name, image, price } = product;
+      return `<article class="product">
+          <div class="product-container">
+            <img src="${image}" class="product-img img" alt="${name}">
+            <!-- product icons -->
+            <div class="product-icons">
+              <a href="product.html?id=${id}" class="product-icon">
+                <i class="fas fa-search"></i>
+              </a>
+              <button class="product-cart-btn product-icon" data-id="${id}">
+                <i class="fas fa-shopping-cart"></i>
+              </button>
+            </div>
+          </div>
+          <footer>
+            <p class="product-name">
+              ${name}
+            </p>
+            <h4 class="product-price">$9.99</h4>
+          </footer>
+        </article>`;
+    })
+    .join("");
+};
+
+export default display;

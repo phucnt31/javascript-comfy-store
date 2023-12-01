a33a2af9cdc8bbb265d4b52ce8460373fc8f9395
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-12-01 11:22:54 +0700

    Add local storage to store products

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
-let store = [];
+import { getStorageItem, setStorageItem } from "./utils.js";
+
+let store = getStorageItem("store");
 
 const setupStore = (products) => {
   store = products.map((product) => {
@@ -9,6 +11,7 @@ const setupStore = (products) => {
     const image = img[0].thumbnails.large.url;
     return { id, featured, name, price, colors, company, image };
   });
+  setStorageItem("store", store);
 };
 
 export { store, setupStore };
--- src/utils.js
+++ src/utils.js
@@ -8,4 +8,16 @@ const getElement = (selection) => {
   );
 };
 
-export { getElement, allProductsUrl };
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item));
+  } else storageItem = [];
+  return storageItem;
+};
+
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
+
+export { getElement, allProductsUrl, getStorageItem, setStorageItem };

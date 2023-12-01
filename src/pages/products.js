e3ade5ec55789601d578a025e025e20566627e55
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-12-01 15:27:52 +0700

    Setup HTML and display products

---
+++ src/pages/products.js
@@ -0,0 +1,11 @@
+// global imports
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
+
+// specific imports
+import { store } from "../store.js";
+import display from "../displayProducts.js";
+import { getElement } from "../utils.js";
+
+display(store, getElement(".products-container"));
